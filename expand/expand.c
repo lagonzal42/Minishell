41fd7e0d373000d526b82e08315472194272334f
Larrain Gonzalez Sebastian <lagonzal@c4r7s3.42urduliz.com>
2023-08-25 13:10:28 +0200

    libs list functions removed in order to quit problems

--- expand/expand.c
+++ expand/expand.c
@@ -6,10 +6,11 @@
 /*   By: lagonzal <lagonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 16:16:20 by lagonzal          #+#    #+#             */
-/*   Updated: 2023/08/25 12:06:53 by lagonzal         ###   ########.fr       */
+/*   Updated: 2023/08/25 13:05:01 by lagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../builtins/builtins.h"
 #include "../libft/libft.h"
 #include "../parse/parse.h"
 #include "expand.h"
@@ -17,6 +18,31 @@
 
 char	**quoute_case(char **spltd, int n, int *m);
 char	**dollar_case(char **spltd, int n, int *m, int quoute);
+
+
+t_env *enviroment;
+
+t_env *get_env(char **envp, t_env *env)
+{
+    t_env   *tmp;
+    int i = 0;
+
+    env = make_node();
+    tmp = env;
+    while (envp[i] != NULL)
+    {
+        tmp->name = ft_substr(envp[i], 0, ft_strchr(envp[i], '=') - envp[i]);
+        tmp->value = ft_substr(envp[i], ft_strchr(envp[i], '=') - envp[i] + 1,
+            ft_strlen(envp[i]));
+        if (envp[++i])
+        {
+            ft_lstadd_back(env, tmp);
+            tmp->next = make_node();
+            tmp = tmp->next;    
+        }
+    }
+    return (env);
+}
 
 char	**expand(char **spltd)
 {
@@ -57,28 +83,30 @@ char **dollar_case(char **spltd, int n, int *m, int quoute)
 {
 	int		prev;
 	char	*holder;
-	char	*left;
-	char	*right;
 	char	*name;
+	char	*to_add[3];
 
 	prev = *m;
 	printf("from prev: %s\n", &spltd[n][*m]);
 	name = ft_substr(spltd[n], *m + 1, find_end_word(spltd, n, *m + 1));
 	ft_printf("name to expand :%s\n", name);
-	left = ft_substr(spltd[n], 0, *m);
-	right = ft_substr(spltd[n], *m + find_end_word(spltd, n, *m), ft_strlen(spltd[n]));
-	printf("left %s\n", left);
-	printf("right %s\n", right);
-	holder = 
-	*m += ft_strlen(name);
-	free(name);
-	free(left);
-	free(right);
-	return (spltd);
+	to_add[0] = ft_substr(spltd[n], 0, *m);
+	to_add[2] = ft_substr(spltd[n], *m + find_end_word(spltd, n, *m), 
+		ft_strlen(spltd[n]));
+	printf("left %s\n", to_add[0]);
+	printf("right %s\n", to_add[2]);
+	to_add[1] = search_for_var(enviroment, name);
+	printf("expanded: %s\n");
+	*m += ft_strlen(to_add[1]) - 1;
+	holder = ft_strjoin(to_add[0], ft_strjoin(to_add[1], to_add[2]));
+	free(spltd[n]);
+	spltd[n] = holder;
+	return (free(to_add[0]), free(to_add[1]), free(to_add[2]), spltd);
 }
 
-int	main(void)
+int	main(int ac, char **av, char **envp)
 {
+	enviroment = get_env(envp, enviroment);
 	char *spltd[] = {"echo", "\"my asingment is to write de user: $USER|mi_file TASK COMPLETED!\"", "|", "cat", "-e", NULL};
 	expand(spltd);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lagonzal <lagonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 10:18:40 by lagonzal          #+#    #+#             */
-/*   Updated: 2023/08/21 12:43:35 by lagonzal         ###   ########.fr       */
+/*   Updated: 2023/08/25 13:08:08 by lagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,6 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstlast(t_list *lst);
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-t_list	*ft_lstnew(void *content);
-int		ft_lstsize(t_list *lst);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
