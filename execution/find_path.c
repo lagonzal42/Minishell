facd530fcd1b685b824b2444a017b08594afb5d3
Otxobot <otxoboy64@gmail.com>
2023-09-05 16:06:29 +0200

    starting redirections

--- execution/find_path.c
+++ execution/find_path.c
@@ -4,12 +4,14 @@ int	main(int ac, char **av, char **envp)
 {
 	int i;
 	int j;
+	int a;
 	char	**split_cmd;
 	char	**bin_paths;
 	char 	*holder;
 
 	i = 0;
 	j = 0;
+	a = 1;
 	if (!av[1])
 		exit(0);
 	if (check_if_path_absolute(av[1]) == 0)
@@ -21,13 +23,26 @@ int	main(int ac, char **av, char **envp)
 	while (env && env->name && ft_strncmp(env->name, "PATH", 4) != 0)
 		env = env->next;
 	bin_paths = ft_split(env->value, ':');
-	while (bin_paths[j])
+	// while (bin_paths[i])
+	// 	printf("%s\n", bin_paths[i++]);
+	while (bin_paths[j] && a == 1)
 	{
 		holder = ft_strjoin(ft_strjoin(bin_paths[j], "/"), split_cmd[i]);
 		j++;
-		execute_commands_path(holder, &av[1], envp);
+		a = check_if_access(holder);
+		if (a == 1)
+			free(holder);
 	}
-	free(holder);
+	if (a == 0)
+		execute_commands_path(holder, &av[1], envp);
+	else
+		ft_putstr_fd("Minishell: command not found", STDERR_FILENO);
 	free(split_cmd);
 	free(bin_paths);
+}
+
+
+void	redirections()
+{
+
 }
