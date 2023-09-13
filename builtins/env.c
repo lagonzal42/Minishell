f065c040772e8736dcee2a306dcbafdf363e906f
Otxobot <otxoboy64@gmail.com>
2023-09-13 16:25:40 +0200

    funcionan las function pointers

--- builtins/env.c
+++ builtins/env.c
@@ -1,11 +1,11 @@
 #include "builtins.h"
 
-void    print_env(t_env *env, char *standardized)
+void    print_env(t_env *env, char **standardized)
 {
-    if (standardized != NULL)
+    if (*standardized != NULL)
     {
         ft_putstr_fd("env: ", STDERR_FILENO);
-        ft_putstr_fd(standardized, STDERR_FILENO);
+        ft_putstr_fd(*standardized, STDERR_FILENO);
         ft_putstr_fd(": No such file or directory", STDERR_FILENO);
     }
     while(env->next != NULL)
